724bb4b3887675c4926c96e2e72eff17fea333cc
davidmueller13 <david.mueller1013@gmail.com>
2015-11-21 20:38:24 +0100

    added stock ramdisk

---
+++ ramdisk/sbin/fix_cache_types.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+echo "temporary none" > /sys/class/scsi_disk/0:0:0:1/cache_type
+echo "temporary none" > /sys/class/scsi_disk/0:0:0:2/cache_type
+echo "temporary none" > /sys/class/scsi_disk/0:0:0:3/cache_type
